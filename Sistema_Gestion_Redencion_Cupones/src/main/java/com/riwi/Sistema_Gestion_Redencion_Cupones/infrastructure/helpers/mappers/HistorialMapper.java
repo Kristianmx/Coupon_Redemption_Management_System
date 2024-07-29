7f28f80f6d35c44d14482f1b4203f49c4286a6a9
Kristiamnx <rivaskristopher24@gmail.com>
2024-07-29 12:36:18 -0500

    feature/US-001-008-Historial-Controller-Services/feat: service completed

--- Sistema_Gestion_Redencion_Cupones/src/main/java/com/riwi/Sistema_Gestion_Redencion_Cupones/infrastructure/helpers/mappers/HistorialMapper.java
+++ Sistema_Gestion_Redencion_Cupones/src/main/java/com/riwi/Sistema_Gestion_Redencion_Cupones/infrastructure/helpers/mappers/HistorialMapper.java
@@ -1,12 +1,25 @@
 package com.riwi.Sistema_Gestion_Redencion_Cupones.infrastructure.helpers.mappers;
 
 import com.riwi.Sistema_Gestion_Redencion_Cupones.api.dtos.requests.HistorialRequest;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.api.dtos.responses.CouponResponse;
 import com.riwi.Sistema_Gestion_Redencion_Cupones.api.dtos.responses.HistorialResponse;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.api.dtos.responses.ProductResponse;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.api.dtos.responses.UserResponse;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.entities.Coupons;
 import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.entities.Historial;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.entities.Product;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.entities.User;
 import com.riwi.Sistema_Gestion_Redencion_Cupones.infrastructure.helpers.generics.GenericMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
+import java.util.List;
+
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface HistorialMapper extends GenericMapper<HistorialRequest, HistorialResponse, Historial> {
+
+    UserResponse toResponseUser(User user);
+    CouponResponse toResponseCoupon(Coupons coupons);
+
+    ProductResponse toResponseProduct(Product product);
 }
--- Sistema_Gestion_Redencion_Cupones/src/main/java/com/riwi/Sistema_Gestion_Redencion_Cupones/infrastructure/services/HistorialService.java
+++ Sistema_Gestion_Redencion_Cupones/src/main/java/com/riwi/Sistema_Gestion_Redencion_Cupones/infrastructure/services/HistorialService.java
@@ -2,12 +2,51 @@ package com.riwi.Sistema_Gestion_Redencion_Cupones.infrastructure.services;
 
 import com.riwi.Sistema_Gestion_Redencion_Cupones.api.dtos.requests.HistorialRequest;
 import com.riwi.Sistema_Gestion_Redencion_Cupones.api.dtos.responses.HistorialResponse;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.entities.Coupons;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.entities.Historial;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.entities.Product;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.entities.User;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.repositories.CouponRepository;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.repositories.HistorialRepository;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.repositories.ProductRepository;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.repositories.UserRepository;
 import com.riwi.Sistema_Gestion_Redencion_Cupones.infrastructure.abstract_services.IHistorialService;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.infrastructure.helpers.mappers.HistorialMapper;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.utils.exceptions.IdNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
 public class HistorialService implements IHistorialService {
+
+    @Autowired
+    private final HistorialMapper historialMapper;
+
+    @Autowired
+    private final HistorialRepository historialRepository;
+    @Autowired
+    private  final UserRepository userRepository;
+    @Autowired
+    private final ProductRepository productRepository;
+    @Autowired
+    private final CouponRepository couponRepository;
     @Override
     public HistorialResponse create(HistorialRequest historialRequest) {
-        return null;
+        Historial historial = this.historialMapper.toEntity(historialRequest);
+        User user = this.userRepository.findById(historialRequest.getUserId()).orElseThrow(()-> new IdNotFoundException("User"));
+        Product product= this.productRepository.findById(historialRequest.getProductsId()).orElseThrow(()->new IdNotFoundException("Product"));
+        Coupons coupons = this.couponRepository.findById(historialRequest.getCuponId()).orElseThrow(()->new IdNotFoundException("Coupon"));
+        historial.setCoupons(coupons);
+        historial.setUser(user);
+        historial.setProduct(product);
+        historial.setStatus(true);
+        historial.setRedemptionDate(LocalDateTime.now());
+
+        return this.historialMapper.toResponse(this.historialRepository.save(historial));
     }
 
     @Override
