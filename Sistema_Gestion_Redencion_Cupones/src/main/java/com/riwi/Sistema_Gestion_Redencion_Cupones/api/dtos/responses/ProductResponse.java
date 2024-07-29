9bccec93da3379b0dd7dadc23f1e89687d06d31c
Dayron-z <zbastian.zapata@gmail.com>
2024-07-29 11:37:20 -0500

    US-001-002-Product Logic /feat: Create Product Dtos, Services and Product Mapper

--- Sistema_Gestion_Redencion_Cupones/src/main/java/com/riwi/Sistema_Gestion_Redencion_Cupones/api/dtos/responses/ProductResponse.java
+++ Sistema_Gestion_Redencion_Cupones/src/main/java/com/riwi/Sistema_Gestion_Redencion_Cupones/api/dtos/responses/ProductResponse.java
@@ -1,13 +1,14 @@
 package com.riwi.Sistema_Gestion_Redencion_Cupones.api.dtos.responses;
 
 import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.entities.Historial;
-import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.entities.Product;
-import jakarta.persistence.*;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponse {
     private Long id;
     private String name;
