388beeb1676a61dea0732d91f428ed01466c7abe
MiguelAngelToro <rengell.angell@gmail.com>
2024-07-29 11:37:38 -0500

    US-001-001-User-Logic/Feat: Creation of the user request and response, IUserService and service

---
+++ Sistema_Gestion_Redencion_Cupones/src/main/java/com/riwi/Sistema_Gestion_Redencion_Cupones/infrastructure/helpers/mappers/UserMapper.java
@@ -0,0 +1,13 @@
+package com.riwi.Sistema_Gestion_Redencion_Cupones.infrastructure.helpers.mappers;
+
+import org.mapstruct.Mapper;
+
+import com.riwi.Sistema_Gestion_Redencion_Cupones.api.dtos.requests.UserRequest;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.api.dtos.responses.UserResponse;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.domain.entities.User;
+import com.riwi.Sistema_Gestion_Redencion_Cupones.infrastructure.helpers.generics.GenericMapper;
+
+@Mapper(componentModel = "spring" )
+public interface UserMapper extends GenericMapper<UserRequest, UserResponse, User> {
+    
+}
